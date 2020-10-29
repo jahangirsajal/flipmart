73971aa17a878ae699a17f98522a544aba96f165
Jahangir Hossain <jahangir.sajal@gmail.com>
2020-10-29 11:50:29 +0600

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['cat_id', 'subcat_id', 'brand_id', 'name', 'slug', 'model', 'buying_price', 'selling_price', 'special_price', 'special_start', 'special_end', 'quantity', 'video_url', 'warranty', 'warranty_duration', 'warranty_conditions', 'thumbnail', 'gallery', 'description', 'long_description', 'status'];
+
+    public const ACTIVE_PRODUCT = 'active';
+    public const INACTIVE_PRODUCT = 'active';
 }
